99bae75226813a88c317df51b085d5bcf1a87e21
Vince Saykao <vtsaykao@hotmail.com>
2022-05-10 12:10:45 -0500

    first commit update

---
+++ Java/freecamp/src/test/java/com/example/freecamp/FreecampApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.freecamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreecampApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
