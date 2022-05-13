21c26fd11f95a57c0ea5d1efd5edc55855d8d4f8
Vince Saykao <vtsaykao@hotmail.com>
2022-05-13 10:29:21 -0500

    created dialog

--- Java/BackToBasicsJava/src/Main.java
+++ Java/BackToBasicsJava/src/Main.java
@@ -1,23 +1,33 @@
 import java.util.Scanner;
 
+import javax.swing.JOptionPane;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello World");
+        // System.out.println("Hello World");
 
-        Scanner scanner = new Scanner(System.in);
+        // Scanner scanner = new Scanner(System.in);
 
         
-        System.out.println("What is your name?");
-        String name = scanner.nextLine();
+        // System.out.println("What is your name?");
+        // String name = scanner.nextLine();
 
-        if (name == "Vince") {
-            
-            System.out.println("Hello" + " " + name);
-        } else {
-                System.out.println("Not Vince");
-        }
+        // System.out.println("Hello" + " " + name);
+
+        String name = JOptionPane.showInputDialog("Enter your name");
+        JOptionPane.showMessageDialog(null,"Hello" + name);
+
+
+        int age = Integer.parseInt(JOptionPane.showInputDialog("Enter your age"));
+        JOptionPane.showMessageDialog(null, "You are " + age);
+
+
+        double height = Double.parseDouble(JOptionPane.showInputDialog("Enter your height"));
+        JOptionPane.showMessageDialog(null, "You are " + height);
+
+
 
     }
 }
