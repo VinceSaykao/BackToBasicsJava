38de31aba592b1fb356d0aba54c177fd08ec11f2
Vince Saykao <vtsaykao@hotmail.com>
2022-05-13 10:00:41 -0500

    added if else statement

--- Java/BackToBasicsJava/src/Main.java
+++ Java/BackToBasicsJava/src/Main.java
@@ -1,7 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
         System.out.println("Hello World");
+
+        Scanner scanner = new Scanner(System.in);
+
+        
+        System.out.println("What is your name?");
+        String name = scanner.nextLine();
+
+        if (name == "Vince") {
+            
+            System.out.println("Hello" + " " + name);
+        } else {
+                System.out.println("Not Vince");
+        }
+
     }
 }
